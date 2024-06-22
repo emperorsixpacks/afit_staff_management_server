00970c3368640c05c58c8250bd86776b74d055c1
emperorsixpacks <andrewdvd10@gmail.com>
2024-06-22 13:35:55 +0100

    update: added phone number validator

--- management_server/models/validators.py
+++ management_server/models/validators.py
@@ -1,4 +1,9 @@
-from management_server.models.helpers import get_mobile_prefix
+from management_server.models.helpers import get_mobile_prefix, MobilePrefix
 
-def phone_number_vaidator(phone_number:str):
-    pass
+def phone_number_vaidator(phone_number:str) -> MobilePrefix | None:
+    mobile_networks = get_mobile_prefix()
+
+    for network in mobile_networks:
+        if phone_number in network.prefixes:
+            return network
+    return None
