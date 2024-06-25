9ce7ece5cb163acb5c01825d83b6a7463ad7374c
emperorsixpacks <andrewdvd10@gmail.com>
2024-06-25 16:26:59 +0100

    update: added create tortoise.yml script

---
+++ scripts/orm.py
@@ -0,0 +1,61 @@
+import os
+import yaml
+
+from management_server.server.settings import DBSettings
+from management_server.constants import APP_BASE_URL
+
+
+CONFIG_FILE = os.path.join(APP_BASE_URL, "extras/tortoise.yml")
+db_settings = DBSettings()
+
+
+DEFAULT_CONFIG = {
+    "connections": {
+        "master": db_settings.database_url
+    },
+    "apps": {
+        "models": {
+            "models": ["management_server.models"],
+            "default_connection": "master",
+        }
+    },
+}
+
+
+def write_config_file(path):
+    """
+    Open a configuration file specified by the `path` using the specified `mode`.
+
+    Parameters:
+        path (str): The path to the configuration file.
+        mode (str): The mode in which the file should be opened. Default is 'r'.
+
+    Returns:
+        TextIOWrapper: The opened file object.
+    """
+    with open(path, mode="w", encoding="utf-8") as f:
+        yaml.dump(DEFAULT_CONFIG, f)
+
+
+def create_config_file():
+    """
+    Create a configuration file if it does not already exist.
+
+    This function checks if the configuration file specified by `CONFIG_FILE` exists. If it does not exist,
+    the function opens the file in read mode and uses the `yaml.dump()` function to write the `DEFAULT_CONFIG`
+    dictionary to the file.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    if os.path.exists(CONFIG_FILE):
+        return
+    write_config_file(CONFIG_FILE)
+    print("Created new config file at", CONFIG_FILE)
+
+
+if __name__ == "__main__":
+    create_config_file()
